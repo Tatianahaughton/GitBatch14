05ab4cca0fe34ec3533392e242db8e04d9afc823
tatianahaughton <tatianahaughton33@gmail.com>
2022-09-27 19:51:51 -0400

    second commit

--- src/gitClass2/Class03.java
+++ src/gitClass2/Class03.java
@@ -3,5 +3,6 @@ package gitClass2;
 public class Class03 {
     public static void main(String[] args) {
         System.out.println("Hey this is a git class");
+        System.out.println("Making a change for git class to see what happens when we make a change to our code");
     }
 }
