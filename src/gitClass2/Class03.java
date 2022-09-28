dd58ec99544cd59ec0d30d16dae1613fd313b068
tatianahaughton <tatianahaughton33@gmail.com>
2022-09-27 20:07:45 -0400

    third commit

--- src/gitClass2/Class03.java
+++ src/gitClass2/Class03.java
@@ -4,5 +4,6 @@ public class Class03 {
     public static void main(String[] args) {
         System.out.println("Hey this is a git class");
         System.out.println("Making a change for git class to see what happens when we make a change to our code");
+        System.out.println("Another change");
     }
 }
